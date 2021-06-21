3641ef0aa8dcb8ca7a75b5969608b3db503155d9
mehrad-bazvand8 <mehradseren8@gmail.com>
2021-06-22 01:26:31 +0430

    update 31

--- src/Component/carousel.js
+++ src/Component/carousel.js
@@ -3,6 +3,8 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./carousel.css";
+import { Route, NavLink, HashRouter, Link, Form } from "react-router-dom";
+
 export default function SimpleSlider() {
   var settings = {
     dots: true,
@@ -16,36 +18,158 @@ export default function SimpleSlider() {
       <div className="carousel">
         <Slider {...settings}>
           <div className="carousel-item">
-            <img src='/img/slideshow1.jpg' className="img-carousel"/>
+            <Link to="/">
+              <div class="card">
+                <img
+                  className="img-carousel"
+                  src="/img/carousel1.jpg"
+                  alt="Denim Jeans"
+                />
+                <span className="carousel-Discount">20%</span>
+                <h1>پیراهن مردانه </h1>
+                <p class="price">$19.99</p>
+                <p class="price-through">$19.99</p>
+              </div>
+            </Link>
           </div>
           <div className="carousel-item">
-          <img src='/img/slideshow1.jpg' className="img-carousel"/>
+            <Link to="/">
+              <div class="card">
+                <img
+                  className="img-carousel"
+                  src="/img/carousel2.jpg"
+                  alt="Denim Jeans"
+                />
+                <span className="carousel-Discount">20%</span>
+                <h1>عینک زنانه مدل ریبون</h1>
+                <p class="price">600،000 تومان</p>
+                <p class="price-through">600،000 تومان</p>
+              </div>
+            </Link>
           </div>
           <div className="carousel-item">
-          <img src='/img/slideshow1.jpg' className="img-carousel"/>
+          <Link to="/">
+            <div class="card">
+              <img
+                className="img-carousel"
+                src="/img/carousel3.jpg"
+                alt="Denim Jeans"
+              />
+              <span className="carousel-Discount">20%</span>
+              <h1>تیشرت مردانه با متن خارجی</h1>
+              <p class="price">150،000 تومان</p>
+              <p class="price-through">150،000 تومان</p>
+            </div>
+            </Link>
           </div>
           <div className="carousel-item">
-          <img src='/img/slideshow1.jpg' className="img-carousel"/>
+          <Link to="/">
+            <div class="card">
+              <img
+                className="img-carousel"
+                src="/img/carousel4.jpg"
+                alt="Denim Jeans"
+              />
+              <span className="carousel-Discount">20%</span>
+              <h1>شلوار زنانه مشکی</h1>
+              <p class="price">300،000 تومان</p>
+              <p class="price-through">300،000 تومان</p>
+            </div>{" "}
+            </Link>
           </div>
           <div className="carousel-item">
-          <img src='/img/slideshow1.jpg' className="img-carousel"/>
+          <Link to="/">
+            <div class="card">
+              <img
+                className="img-carousel"
+                src="/img/carousel5.jpg"
+                alt="Denim Jeans"
+              />
+              <span className="carousel-Discount">20%</span>
+              <h1>پماد زد افتاب زنانه</h1>
+              <p class="price">900،000 تومان</p>
+              <p class="price-through">900،000 تومان</p>
+            </div>{" "}
+            </Link>
           </div>
           <div className="carousel-item">
-          <img src='/img/slideshow1.jpg' className="img-carousel"/>
+          <Link to="/">
+            <div class="card">
+              <img
+                className="img-carousel"
+                src="/img/carousel6.jpg"
+                alt="Denim Jeans"
+              />
+              <span className="carousel-Discount">20%</span>
+              <h1>لاک اکلیلی قهوه ای</h1>
+              <p class="price">50،000 تومان</p>
+              <p class="price-through">120،000 تومان</p>
+            </div>{" "}
+            |</Link>
           </div>
           <div className="carousel-item">
-          <img src='/img/slideshow1.jpg' className="img-carousel"/>
+          <Link to="/">
+            <div class="card">
+              <img
+                className="img-carousel"
+                src="/img/carousel8.jpg"
+                alt="Denim Jeans"
+              />
+              <span className="carousel-Discount">20%</span>
+              <h1>تیشرت بچگانه سه رنگ </h1>
+              <p class="price">70،000 تومان</p>
+              <p class="price-through">120،000 تومان</p>
+            </div>{" "}
+            </Link>
           </div>
           <div className="carousel-item">
-          <img src='/img/slideshow1.jpg' className="img-carousel"/>
+          <Link to="/">
+            <div class="card">
+              <img
+                className="img-carousel"
+                src="/img/carousel9.jpg"
+                alt="Denim Jeans"
+              />
+              <span className="carousel-Discount">20%</span>
+              <h1>بولیز زنانه سفید</h1>
+              <p class="price">120،000 تومان</p>
+              <p class="price-through">150،000 تومان</p>
+            </div>
+            </Link>
           </div>
           <div className="carousel-item">
-          <img src='/img/slideshow1.jpg' className="img-carousel"/>
+          <Link to="/">
+            <div class="card">
+              <img
+                className="img-carousel"
+                src="/img/carousel4.jpg"
+                alt="Denim Jeans"
+              />
+              <span className="carousel-Discount">20%</span>
+              <h1>شلوار زنانه</h1>
+              <p class="price"> تومان 200،0000</p>
+              <p class="price-through">300،000 تومان</p>
+            </div>
+            </Link>
           </div>
           <div className="carousel-item">
-          <img src='/img/slideshow1.jpg' className="img-carousel"/>
+          <Link to="/www.google.com">
+            <div class="card">
+              <img
+                className="img-carousel"
+                src="img/carousel9.jpg"
+                alt="Denim Jeans"
+              />
+              <span className="carousel-Discount">20%</span>
+
+              <h1>بولیز زنانه سفید</h1>
+              <p class="price">120،000 تومان</p>
+              <p class="price-through">140،000 تومان</p>
+            </div>
+            </Link>
           </div>
         </Slider>
+        <Link to="/"><button className="btn-carousel">مشاهده همه</button></Link>
       </div>
     </div>
   );
---
+++ src/Component/categorytabs.js
@@ -0,0 +1,15 @@
+import React from "react";
+function Categorytabs() {
+  return (
+    <>
+      <div className="container">
+        <div className="tabset">
+          <label for="tab2">مشخصات محصول</label>
+
+          <div class="tab-panels"></div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Categorytabs;
